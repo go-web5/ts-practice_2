abd7ad6c7a65b386e6f5b3fd48fd5d949d38ee17
go-web5 <go.web.creator@gmail.com>
2024-05-31 23:07:27 +0900

    クラスを用いたオブジェクト生成、コンストラクタについて

--- chapter4.ts
+++ chapter4.ts
@@ -1,4 +1,6 @@
-// インターフェイス
+//*------------------------------------
+// 4-1 インターフェイス
+//*------------------------------------
 // オブジェクトの形状を明確に定義し、その形状に合致するオブジェクトのみを扱うことを強制できる
 
 // 型エイリアスによるオブジェクト型の宣言
@@ -185,3 +187,64 @@ const myCarA: CarTypeC = {
 // 型エイリアス
 // ユニオン型、インターセクション型など、任意の型を組み合わせることで、より複雑な型を表現できる
 // 上記の点において、インターフェイスより汎用的
+
+
+//*------------------------------------
+// 4-2 クラス
+//*------------------------------------
+// クラスを使うことで、再利用可能なオブジェクトのテンプレートを作成し、一貫性のあるオブジェクト構想を容易に実現できるようになる
+
+// クラスによるオブジェクトの作成
+class PersonClassA {
+  // プロパティをクラスのトップレベルで宣言する、しないとエラー出る
+  name: string;
+  age: number;
+  hobbies?: string[]; // オプショナルプロパティ
+
+  // コンストラクタのパラメータには型注釈が必要
+  // コンストラクタの戻り値の方は指定しない
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+    
+  }
+  
+  // greet メソッドを追加
+  greet(greeting: string): void {
+    console.log(
+      `${greeting}, my name is ${this.name} and I'm ${this.age} years old.`
+    );
+  }
+}
+
+const takeshi = new PersonClassA("takeshi suzuki", 30);
+
+// greet メソッドを呼び出し
+takeshi.greet("Hello!!")
+
+// 型としてクラスを使用
+let taro: PersonClassA;
+// クラスの左に new をつける 
+taro = new PersonClassA("taro yamada", 50);
+taro.greet("What's up!!")
+
+
+// PersonClassB 型の変数 hiroshi にオブジェクトリテラルで生成したオブジェクトを代入する
+// オブジェクトの構造的型付け
+class PersonClassB {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+let hiroshi: PersonClassB;
+// オブジェクトリテラルを使用してオブジェクトを生成する。new は必要ない
+hiroshi = {
+  name: "hiroshi suzuki",
+  age: 100
+}
+// クラスから生成したオブジェクトを変数に代入した場合、その変数の型はそのクラスの型になる
